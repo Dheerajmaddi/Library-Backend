59bbfb0ee231b10f516ee01abcd2a801e666e596
Dheeraj Maddi <dheerajmaddi@gmail.com>
2024-08-01 22:13:03 -0500

    Added another query to filter books by its category.

--- src/main/java/com/dmaddi/spring_boot_library/dao/BookRepository.java
+++ src/main/java/com/dmaddi/spring_boot_library/dao/BookRepository.java
@@ -6,8 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
+
+    Page<Book> findByCategory(@RequestParam("category") String category, Pageable pageable);
 }
