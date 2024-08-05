3369b3d0f34b85c86481a5b92ee227838c73d053
Dheeraj Maddi <dheerajmaddi@gmail.com>
2024-08-04 20:47:01 -0500

    Added a Review repository to enable api calls to the my sql database. Also added an additional functionality to search review based on bookId.

---
+++ src/main/java/com/dmaddi/spring_boot_library/dao/ReviewRepository.java
@@ -0,0 +1,12 @@
+package com.dmaddi.spring_boot_library.dao;
+
+import com.dmaddi.spring_boot_library.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findBookById(@RequestParam("bookId") Long bookId, Pageable pageable);
+}
