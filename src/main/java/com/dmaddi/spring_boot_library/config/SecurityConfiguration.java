15fda5c8046b3eff2023222692bbf7c91c9f933e
Dheeraj Maddi <dheerajmaddi@gmail.com>
2024-08-12 22:49:53 -0500

    Changed the configuration code with modern implementation.

--- src/main/java/com/dmaddi/spring_boot_library/config/SecurityConfiguration.java
+++ src/main/java/com/dmaddi/spring_boot_library/config/SecurityConfiguration.java
@@ -1,4 +1,45 @@
 package com.dmaddi.spring_boot_library.config;
+//
+//import com.okta.spring.boot.oauth.Okta;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.web.accept.ContentNegotiationStrategy;
+//import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+//
+//@Configuration
+//public class SecurityConfiguration {
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        // Disable Cross Site Request Forgery
+//
+//        http.csrf().disable();
+//
+//        // Add cors filter
+//        http.cors();
+//
+//        // Protect endpoints at api/<type>/secure
+//        http.authorizeHttpRequests(configurer ->
+//                        configurer.requestMatchers("/api/books/secure/**", "/api/reviews/secure/**")
+//                                .authenticated()
+//                                .requestMatchers("/api/books/**", "/api/reviews/**")
+//                                .permitAll())
+//                .oauth2ResourceServer().jwt();
+//
+//
+//        // Add content negotiation strategy
+//        http.setSharedObject(ContentNegotiationStrategy.class,
+//                new HeaderContentNegotiationStrategy());
+//
+//        // Force a non-empty response body to make unauthorized response body more friendly.
+//        Okta.configureResourceServer401ResponseBody(http);
+//        return http.build();
+//
+//    }
+//}
+
 
 import com.okta.spring.boot.oauth.Okta;
 import org.springframework.context.annotation.Bean;
@@ -15,29 +56,30 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         // Disable Cross Site Request Forgery
+        http.csrf(AbstractHttpConfigurer::disable);
 
-        http.csrf().disable();
-
-        // Add cors filter
-        http.cors();
-
-        // Protect endpoints at api/<type>/secure
+        // Protect endpoints at /api/<type>/secure
         http.authorizeHttpRequests(configurer ->
-                        configurer.requestMatchers("/api/books/secure/**", "/api/reviews/secure/**")
-                                .authenticated()
-                                .requestMatchers("/api/books/**", "/api/reviews/**")
-                                .permitAll())
-                .oauth2ResourceServer().jwt();
+                        configurer
+                                .requestMatchers("/api/books/secure/**",
+                                        "/api/reviews/secure/**")
+                                .authenticated().anyRequest().permitAll())
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()));
 
+
+        // Add CORS filters
+        http.cors(Customizer.withDefaults());
 
         // Add content negotiation strategy
         http.setSharedObject(ContentNegotiationStrategy.class,
                 new HeaderContentNegotiationStrategy());
 
-        // Force a non-empty response body to make unauthorized response body more friendly.
+        // Force a non-empty response body for 401's to make the response friendly
         Okta.configureResourceServer401ResponseBody(http);
-        return http.build();
 
+        return http.build();
     }
+
 }
