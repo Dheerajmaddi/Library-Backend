2c5ba002f65fef8d5af3cebebfecd87e9bcbd929
Dheeraj Maddi <dheerajmaddi@gmail.com>
2024-08-14 18:20:06 -0500

    Added a new message control to create apis which help in communicating with the service class of message.

---
+++ src/main/java/com/dmaddi/spring_boot_library/controller/MessagesController.java
@@ -0,0 +1,28 @@
+package com.dmaddi.spring_boot_library.controller;
+
+import com.dmaddi.spring_boot_library.entity.Message;
+import com.dmaddi.spring_boot_library.service.MessagesService;
+import com.dmaddi.spring_boot_library.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+@RequestMapping("/api/messages")
+public class MessagesController {
+
+    private MessagesService messagesService;
+
+    @Autowired
+    public MessagesController(MessagesService messagesService){
+        this.messagesService = messagesService;
+    }
+
+    @PostMapping("/secure/add/message")
+    public void postMessage(@RequestHeader(value = "Authorization") String token,
+                            @RequestBody Message messageRequest){
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        messagesService.postMessage(messageRequest, userEmail);
+    }
+
+}
