9c252a2c91cddc8b8232975070234e890378d928
Dheeraj Maddi <dheerajmaddi@gmail.com>
2024-08-15 21:24:08 -0500

    Exposed ids for Message class and disabled unnecessary http methods.

--- src/main/java/com/dmaddi/spring_boot_library/config/MyDataRestConfig.java
+++ src/main/java/com/dmaddi/spring_boot_library/config/MyDataRestConfig.java
@@ -1,6 +1,7 @@
 package com.dmaddi.spring_boot_library.config;
 
 import com.dmaddi.spring_boot_library.entity.Book;
+import com.dmaddi.spring_boot_library.entity.Message;
 import com.dmaddi.spring_boot_library.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
@@ -19,9 +20,11 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         HttpMethod[] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PATCH, HttpMethod.DELETE, HttpMethod.PUT};
         config.exposeIdsFor(Book.class);
         config.exposeIdsFor(Review.class);
+        config.exposeIdsFor(Message.class);
 
         disableHttpMethods(Book.class, config, theUnsupportedActions);
         disableHttpMethods(Review.class, config, theUnsupportedActions);
+        disableHttpMethods(Message.class, config, theUnsupportedActions);
 
         /* Configure CORS Mapping */
         cors.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrigins);
