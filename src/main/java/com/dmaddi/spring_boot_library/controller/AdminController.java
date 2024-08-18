26baac16c6cbdc3b55ba3f87a0cdbb7c901db7c3
Dheeraj Maddi <dheerajmaddi@gmail.com>
2024-08-18 14:10:20 -0500

    Added an AdminController which is connected to the postBook method of the AdminService via a POST mapping endpoint "/secure/add/book".

---
+++ src/main/java/com/dmaddi/spring_boot_library/controller/AdminController.java
@@ -0,0 +1,32 @@
+package com.dmaddi.spring_boot_library.controller;
+
+import com.dmaddi.spring_boot_library.requestmodels.AddBookRequest;
+import com.dmaddi.spring_boot_library.service.AdminService;
+import com.dmaddi.spring_boot_library.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+@RequestMapping("/api/admin")
+public class AdminController {
+
+    private AdminService adminService;
+
+    @Autowired
+    public AdminController(AdminService adminService){
+        this.adminService = adminService;
+    }
+
+    @PostMapping("/secure/add/book")
+    public void postBook(@RequestHeader(value = "Authorization") String token,
+                         @RequestBody AddBookRequest addBookRequest) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+
+        if(admin == null || !admin.equals("admin")){
+            throw new Exception("Administration page only");
+        }
+
+        adminService.postBook(addBookRequest);
+    }
+}
